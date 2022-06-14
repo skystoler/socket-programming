b4aeef134533c69fec94e55f5642bde6f4ee7ff6
skystoler <982932529@qq.com>
2022-06-14 13:07:16 +0800

    Update linux_http_server.c

--- http_server/linux_http_server.c
+++ http_server/linux_http_server.c
@@ -1,1 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
 
+int main(int argc,char* argv[]){
+    if(argc!=2){
+        printf("port<%s> used\n",argv[0]);
+        exit(1);
+    }
+    return 0;
+}
